26e74a5c783dc4c3c44e421e9fed292c47119e98
Igor Potyomkin <igor.potyomkin@inventorsoft.co>
2021-10-05 15:44:01 +0300

    - Some changes;

--- src/main/java/com/testgit/gitdemo/teste32.java
+++ src/main/java/com/testgit/gitdemo/teste32.java
@@ -3,5 +3,8 @@ package com.testgit.gitdemo;
 public class teste32 {
    private Integer one;
    private Integer second;
+   private Integer second2;
+   private Integer second3;
+   private Integer second;
    private Integer third;
 }
